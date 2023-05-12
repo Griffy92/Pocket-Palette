336fc590400613562ee5e4b4ec10a00751565071
Dominic <dominicwong09@gmail.com>
2023-05-12 20:18:00 +1200

    Canvas/Circle component created

---
+++ src/components/Circle.jsx
@@ -0,0 +1,13 @@
+const Circle = () => {
+
+    const circle = new fabric.Circle({
+        height: 200,
+        width: 200,
+        fill: 'yellow'
+    });
+
+    return (
+        <>
+        </>
+    )
+};
