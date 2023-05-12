a5c726651b0c5e2b1cad4c767cec5aa4da2e39fc
Dominic <dominicwong09@gmail.com>
2023-05-13 00:38:21 +1200

    Circle and Ellipse buttons completed

--- src/components/Circle.jsx
+++ src/components/Circle.jsx
@@ -1,13 +1,26 @@
-const Circle = () => {
+import { fabric } from 'fabric'
 
-    const circle = new fabric.Circle({
-        height: 200,
-        width: 200,
-        fill: 'yellow'
-    });
+const Circle = ( props ) => {
+
+    const { canvas } = props; // destructuring props
+    
+    const _handleAddCircle = () => {
+        const circle = new fabric.Circle({
+            radius: 50,
+            fill: 'red'
+        });
+
+        // console.log(circle); // circle needs radius defined
+
+        canvas.add(circle);
+        canvas.renderAll();
+    };
 
     return (
         <>
+            <button onClick={ _handleAddCircle }>Circle</button>
         </>
     )
 };
+
+export default Circle;
