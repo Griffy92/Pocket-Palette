70af6a7b909b43fb7bb2a9e7c8395cd718077b8b
Roxanne Liu <roxanneliu819@gmail.com>
2023-05-15 11:04:54 +1000

    Text page worksgit add .

--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -16,8 +16,61 @@ const Text = ( props ) => {
     return (
         <>
             <button className="fontButton" onClick={ _handleAddText } title="Add Text">T</button>
+            <button className="fontButton" onClick={ _handleToggleBold } title="Bold">B</button>
+            <button className="fontButton fontItalics" onClick={ _handleToggleItalic } title="Italics">I</button>
         </>
     );
+
+    function _handleToggleBold() {
+        let activeInstance = canvas.getActiveObject();
+        if (activeInstance && activeInstance.type === 'i-text') {
+            if (activeInstance.selectionStart !== activeInstance.selectionEnd) {
+                let startIndex = activeInstance.selectionStart;
+                let endIndex = activeInstance.selectionEnd;
+                let styles = activeInstance.getSelectionStyles(startIndex, endIndex);
+                let fontWeightSet = [...new Set(styles.map(style => style.fontWeight))];
+                if (fontWeightSet.includes("bold")) {
+                    activeInstance.setSelectionStyles({ 'fontWeight': 'normal'})
+                } else {
+                    activeInstance.setSelectionStyles({ 'fontWeight': 'bold'})
+                }
+            } else {
+                if (activeInstance.fontWeight === 'bold') {
+                    activeInstance.fontWeight = 'normal';
+                } else {
+                    activeInstance.fontWeight = 'bold';
+                }
+            }
+        }
+        canvas.renderAll();
+    }
+
+
+
+    function _handleToggleItalic() {
+        let activeInstance = canvas.getActiveObject();
+        if (activeInstance && activeInstance.type === 'i-text') {
+            if (activeInstance.selectionStart !== activeInstance.selectionEnd) {
+                let startIndex = activeInstance.selectionStart;
+                let endIndex = activeInstance.selectionEnd;
+                let styles = activeInstance.getSelectionStyles(startIndex, endIndex);
+                let fontStyleSet = [...new Set(styles.map(style => style.fontStyle))];
+                if (fontStyleSet.includes("italic")) {
+                    activeInstance.setSelectionStyles({ 'fontStyle': 'normal'})
+                } else {
+                    activeInstance.setSelectionStyles({ 'fontStyle': 'italic'})
+                }
+            } else {
+                if (activeInstance.fontStyle === 'italic') {
+                    activeInstance.fontStyle = 'normal';
+                } else {
+                    activeInstance.fontStyle = 'italic';
+                }
+            }
+        }
+        canvas.renderAll(); 
+    }
+    
 }
 
 export default Text;
