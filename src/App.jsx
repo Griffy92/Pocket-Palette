9632996a5643eb3a07a25fa954ab2d2867b901b2
Stuart Cannone <stuartcannone@gmail.com>
2023-05-15 15:40:20 +1000

    added user signup and login

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import Canvas from './components/Canvas'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './components/Login'
+import Success from './components/Success'
 function App() {
 	return (
-		<>
-			<div>
-        		<Canvas />
-			</div>
-		</>
+		<div>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={ <Login/> } />
+				<Route path="/success" element={ <Success /> } />
+				
+			</Routes>
+
+		</BrowserRouter>
+		
+		</div>
 	)
 }
 export default App
