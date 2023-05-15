ec085d65bc048ae986676401a6515dc67d234c69
Roxanne Liu <roxanneliu819@gmail.com>
2023-05-15 19:42:20 +1000

    Fix path

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -3,12 +3,11 @@ import { createClient } from "@supabase/supabase-js"
 
 import { useNavigate } from "react-router-dom"
 import { useEffect, useState } from "react"
-import Canvas from "./Canvas"
+import Canvas from "./Paint/Canvas"
 
 const supabase = createClient (
     "https://yilvmajrrjkkeljiduxi.supabase.co",
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlpbHZtYWpycmpra2VsamlkdXhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQxMDY1NzMsImV4cCI6MTk5OTY4MjU3M30.kACu74ur-SSSo26idj89j46s2TytpXjaf3sMjHulBnM"
-
 )
 
 
