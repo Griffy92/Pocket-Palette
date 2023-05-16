2f35e9e886cc4db504dc9aac5213d17d074db0f1
kaarenliangg <karenliang9546@gmail.com>
2023-05-16 21:15:37 +1000

    pixel art separated components. Refining the CSS.

--- src/components/PixelArt/PixelCanvas.jsx
+++ src/components/PixelArt/PixelCanvas.jsx
@@ -1,20 +1,70 @@
-// import { fabric } from 'fabric';
-import { useState, useEffect } from 'react';
-// import 'fabric-history';
+import React, { useRef, useState } from "react";
+import { Dotting } from "dotting";
 
 
-
-import Grid from './Grid';
+// COMPONENTS
 import PixelToolbar from './PixelToolbar';
 
-  const PixelCanvas = () => {
-    return (
-      <>
-        <Grid />
-        <PixelToolbar />
-      </>
-    );
-  };
-  
+// Renders the board and toolbar for pixel art
 
+const PixelCanvas = () => {
+    // dotting documentation - direct acces to DOM element
+    const ref = useRef(null);
+
+    // setting row and column
+    const [row, setRow ] = useState(800);
+    const [column, setColumn] = useState(800);
+    
+    // handles row and column changes and converts to a number
+    const _handleRowChange = (e) => {
+        setRow(Number(e.target.value));
+      };
+    
+      const _handleColumnChange = (e) => {
+        setColumn(Number(e.target.value));
+      };
+
+    return (
+        <div>
+
+            {/*  Row input */}
+            <div>
+                <label htmlFor="rows">Rows:</label>
+                <input
+                    id="rows"
+                    type="number"
+                    min="100"
+                    value={row}
+                    onChange={_handleRowChange}
+                />
+            </div>
+
+            {/*  Column input */}
+            <div>
+                <label htmlFor="columns">Columns:</label>
+                <input
+                    id="columns"
+                    type="number"
+                    min="100"
+                    value={column}
+                    onChange={_handleColumnChange}
+                />
+            </div>
+
+            {/*  Importing the toolbar */}
+            <PixelToolbar canvas={ ref } />
+
+            {/*  Rendering the Board */}
+            <Dotting 
+                className="pixel-board"
+                ref={ref} 
+                width={row} 
+                height={column}
+                isGridVisible
+            />
+
+        </div>
+    );
+};
+  
 export default PixelCanvas;
