e4477e7326278b1d60908224b5865c044dee5321
EdgarAllenExile <riley.mercer4@gmail.com>
2023-05-16 09:55:36 +0800

    nothing important

--- src/components/Paint/Free-Draw.jsx
+++ src/components/Paint/Free-Draw.jsx
@@ -4,9 +4,9 @@ import Popover from '@mui/material/Popover';
 const FreeDraw = ( props ) => {
     const { canvas, colour, brushSize } = props
 
-    if (canvas != null){
-        canvas.isDrawingMode = false
-      };
+    // if (canvas != null){
+    //     canvas.isDrawingMode = false
+    //   };
 
     // This is a little checker to see if a colour has been passed yet (colour default is "")
     // if a colour has not been passed, defaults to whatver colour is nominated below 
@@ -24,19 +24,31 @@ const FreeDraw = ( props ) => {
     const _handleAddFreeDraw = () => {
         canvas.freeDrawingBrush.color = fillColour
         canvas.freeDrawingBrush.width = fillBrushSize
-        canvas.isDrawingMode = !canvas.isDrawingMode     
+        canvas.isDrawingMode = !canvas.isDrawingMode
+        // isDrawOn() 
+        // FreeDraw(props)
+        // console.log(isDrawOn())     
     };
-
 
     ////////////////////////////////////// This shit does not work - and I honestly cannot fix it.
     // const _handleAddFreeEraser = () => {
     //     canvas.freeDrawingBrush = new fabric.EraserBrush(canvas);
     //     canvas.isDrawingMode = true;
-    // };
-    
+    // }:
+
+    // const isDrawOn = () => {
+    //     if ((canvas != null) && canvas.isDrawingMode) {
+    //         return "active_tool_icon"
+    //     }
+    //     return ""
+    // }
+
+    // console.log(isDrawOn())
+
+
     return (
         <>
-            <button className="add_button add_free_draw" onClick= { _handleAddFreeDraw } title="Draw"></button>
+            <button className={`add_button add_free_draw`} onClick={ _handleAddFreeDraw } title="Draw"></button>
             {/* <button className="add_button add_free_draw" onClick= { _handleAddFreeEraser } title="Draw"></button> */}
         </>
     )
