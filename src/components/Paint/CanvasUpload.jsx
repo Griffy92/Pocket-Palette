34e9fcec4166f96a32fc01fc6630d3961656575e
Stuart Cannone <stuartcannone@gmail.com>
2023-05-18 10:43:44 +1000

    edits to works functionality

--- src/components/Paint/CanvasUpload.jsx
+++ src/components/Paint/CanvasUpload.jsx
@@ -1,37 +1,36 @@
-import { Button, TextField } from '@mui/material';
+import { Button } from '@mui/material';
 import { useState, useEffect } from 'react';
 import { createClient } from '@supabase/supabase-js';
+import { v4 as uuidv4 } from 'uuid';
 
 const supabase = createClient(
   "https://yilvmajrrjkkeljiduxi.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlpbHZtYWpycmpra2VsamlkdXhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQxMDY1NzMsImV4cCI6MTk5OTY4MjU3M30.kACu74ur-SSSo26idj89j46s2TytpXjaf3sMjHulBnM"
 );
 
-const CanvasUpload = (props) => {
-  const [fileName, setFileName] = useState('');
+const CanvasUpload = () => {
   const [userId, setUserId] = useState('');
-  const [imageUrl, setImageUrl] = useState('');
 
   const handleCanvasUpload = async () => {
-    if (fileName && userId) {
-      const canvas = document.getElementById('canvas');
-      canvas.toBlob(async (blob) => {
-        if (blob) {
-          const { error } = await supabase.storage
-            .from(`Works/${userId}`)
-            .upload(`${fileName}.png`, blob, {
-              cacheControl: 'max-age=31536000',
-              contentType: 'image/png',
-            });
+    const fileName = uuidv4();
 
-          if (error) {
-            console.error('Error uploading canvas:', error);
-          } else {
-            console.log('Canvas uploaded successfully');
-          }
+    const canvas = document.getElementById('canvas');
+    canvas.toBlob(async (blob) => {
+      if (blob) {
+        const { error } = await supabase.storage
+          .from(`Works/${userId}`)
+          .upload(`${fileName}.png`, blob, {
+            cacheControl: 'max-age=31536000',
+            contentType: 'image/png',
+          });
+
+        if (error) {
+          console.error('Error uploading canvas:', error);
+        } else {
+          console.log('Canvas uploaded successfully');
         }
-      }, 'image/png');
-    }
+      }
+    }, 'image/png');
   };
 
   const getUser = async () => {
@@ -52,15 +51,10 @@ const CanvasUpload = (props) => {
 
   return (
     <>
-      <TextField
-        label="File Name"
-        value={fileName}
-        onChange={(e) => setFileName(e.target.value)}
-      />
       <Button onClick={handleCanvasUpload}>Upload</Button>
-
     </>
   );
 };
 
 export default CanvasUpload;
+
