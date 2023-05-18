8c16f340a52a0d2274ed52dad7a7906e22788598
Dominic <dominicwong09@gmail.com>
2023-05-18 12:52:40 +1200

    homepage css

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,44 +1,43 @@
-import { createClient } from "@supabase/supabase-js"
-import { Auth } from "@supabase/auth-ui-react"
-import { ThemeSupa } from '@supabase/auth-ui-shared'
-import { useNavigate } from "react-router-dom"
-import { useEffect } from "react"
+import { createClient } from "@supabase/supabase-js";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from '@supabase/auth-ui-shared';
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import './Login.css';
+
 const supabase = createClient (
     "https://yilvmajrrjkkeljiduxi.supabase.co",
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlpbHZtYWpycmpra2VsamlkdXhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQxMDY1NzMsImV4cCI6MTk5OTY4MjU3M30.kACu74ur-SSSo26idj89j46s2TytpXjaf3sMjHulBnM"
-
-)
+);
 
 function Login() {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
     
     useEffect(() => {
         async function getLogin() {
-        supabase.auth.onAuthStateChange(async (event) => {
-        if (event !== "SIGNED_OUT") {
-            navigate("/")
-        }
-        if (event === "SIGNED_IN") {
-            navigate("/success")
-        }
-    
-    })
-        }
-    getLogin()
-    }, [])
+            supabase.auth.onAuthStateChange(async (event) => {
+                if (event !== "SIGNED_OUT") {
+                    navigate("/");
+                };
+
+                if (event === "SIGNED_IN") {
+                    navigate("/success");
+                };
+            });
+        };
+        getLogin();
+    },[]);
 
 	return (
-		<div>
-			<div>   
-                <h1>Welcome to Pocket Palette </h1>
-                <div className="login">
+		<div className="main-login">
+            <div className='main-image'></div>
+			<div className='main-form'>   
                 <Auth 
                 supabaseClient={supabase}
                 appearance={{theme: ThemeSupa}}
                 theme="dark"
                 providers={["email"]}
                 />
-                </div>
 			</div>
 		</div>
 	)
