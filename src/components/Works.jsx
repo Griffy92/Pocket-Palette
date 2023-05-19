499866ba6fcb003ac5c0960402fe335db1cfdb3c
Dominic <dominicwong09@gmail.com>
2023-05-19 20:21:47 +1200

    fetch session in works component

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,5 +1,3 @@
-import { Auth } from '@supabase/auth-ui-react'
-import {ThemeSupa} from '@supabase/auth-ui-shared'
 import { useEffect, useState } from "react";
 import { createClient } from '@supabase/supabase-js'
 import { v4 as uuidv4 } from 'uuid';
@@ -10,150 +8,115 @@ const supabase = createClient (
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlpbHZtYWpycmpra2VsamlkdXhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQxMDY1NzMsImV4cCI6MTk5OTY4MjU3M30.kACu74ur-SSSo26idj89j46s2TytpXjaf3sMjHulBnM"
     )
 
-function Works() {
+function Works( props ) {
+    const [media, setMedia] = useState([]);
+    const navigate = useNavigate();
+    const { session } = props;
+    const { user } = session; // destructure session
 
-const [userId, setUserId] = useState('');
-const [media, setMedia] = useState([]);
-const navigate = useNavigate()
-
-const getUser = async () => {
-
-    try {
-    const { data: { user } } = await supabase.auth.getUser()
-    if (user !== null) {
-        setUserId(user.id);
-        console.log(user.id)
-    } else {
-        setUserId('');
+    useEffect ( () => {
+        if ( !session ) {
+            navigate("/")
+        } else {
+            getMedia();
         }
-    } catch (e) { }
-
-    }
-
+    }, [session])
 
     async function uploadImage(e) {
-    let file = e.target.files[0];
+        let file = e.target.files[0];
 
+        const { data, error } = await supabase
+            .storage
+            .from('Works')
+            .upload(user.id + "/" + uuidv4(), file)
 
-    const { data, error } = await supabase
-        .storage
-        .from('Works')
-        .upload(userId + "/" + uuidv4(), file)
-
-    if (data) {
-        getMedia();
-
-    } else {
-        console.log(error);
-    }
-    }
+        if (data) {
+            getMedia();
+        } else {
+            console.log(error);
+        };
+    };
 
     async function getMedia() {
+        const { data, error } = await supabase.storage.from('Works').list(user.id + '/', {
+            limit: 20,
+            offset: 0,
+            sortBy: {
+            column: 'created_at', 
+            order: 'desc'
+            }
+        });
 
-    const { data, error } = await supabase.storage.from('Works').list(userId + '/', {
-        limit: 20,
-        offset: 0,
-        sortBy: {
-        column: 'created_at', 
-        order: 'desc'
-        }
-    });
-
-    if (data) {
-        setMedia(data);
-    } else {
-        console.log(71, error);
-    }
-    }
-
-    async function signOutUser() {
-        const {error} = await supabase.auth.signOut();
-        console.log(error)
-        navigate("/")
-    }
-
-    useEffect(() => {
-    getUser();
-    getMedia();
-    }, [userId])
+        if (data) {
+            setMedia(data);
+        } else {
+            console.log(71, error);
+        };
+    };
 
     async function deleteMedia(mediaName) {
         const { error } = await supabase
             .storage
             .from('Works')
-            .remove([ userId + "/" + mediaName])
+            .remove([ user.id + "/" + mediaName])
         
         if(error) {
             alert(error);
         } else {
             getMedia();
-        }
-        }
+        };
+    };
+
     async function downloadMedia(mediaName) {
         const { data, error } = await supabase
             .storage
             .from('Works')
-            .download(userId + "/" + mediaName);
+            .download(user.id + "/" + mediaName);
 
             if (data) {
-              // Create a download link
-            const downloadUrl = URL.createObjectURL(data);
+                // Create a download link
+                const downloadUrl = URL.createObjectURL(data);
 
-              // Create an anchor element and set its href and download attributes
-            const link = document.createElement("a");
-            link.href = downloadUrl;
-            link.download = mediaName;
+                // Create an anchor element and set its href and download attributes
+                const link = document.createElement("a");
+                link.href = downloadUrl;
+                link.download = mediaName;
 
-            // Simulate a click on the anchor element to trigger the download
-            link.click();
+                // Simulate a click on the anchor element to trigger the download
+                link.click();
 
-            // Clean up the URL object
-            URL.revokeObjectURL(downloadUrl);
+                // Clean up the URL object
+                URL.revokeObjectURL(downloadUrl);
             } else {
-            console.log(error);
-            }
-        }
+                console.log(error);
+            };
+        };
+
     return (
-            <div>
-                {userId === '' ? (
-                    <Auth
-                    supabaseClient={supabase}
-                    appearance={{ theme: ThemeSupa }}
-                    />
-            ) 
-            : 
-            (
-                <div>
-                    
-                    <div>
-                        <div className="header-container">
-                            <h1>My Gallery</h1>
-                                <label className="file-input">
-                                    Select a file to Upload
-                                <input type="file" onChange={(e) => uploadImage(e)} />
-                                </label>
-                        </div>
-                            <div className="grid-container">
-                            {media.map((media, i) => (
-                            <div key={i} className="grid-item">
-                                    <img src={`https://yilvmajrrjkkeljiduxi.supabase.co/storage/v1/object/public/Works/${userId}/${media.name}`}
-                                            onClick={() => window.open(`https://yilvmajrrjkkeljiduxi.supabase.co/storage/v1/object/public/Works/${userId}/${media.name}`, '_blank')}/>
-                                <div className="button-container">
-                                <button className="delete-button" onClick={() => deleteMedia(media.name)}>Delete Image</button>
-                                <button className="download-button" onClick={() => downloadMedia(media.name)}>Download Image</button>
-                                </div>
-                            </div>
-                            ))}
-                            </div>
-                    </div>
-                </div>  
-            )}
             
+        <div>
+            <div className="header-container">
+                <h1>My Gallery</h1>
+                    <label className="file-input">
+                        Select a file to Upload
+                    <input type="file" onChange={(e) => uploadImage(e)} />
+                    </label>
             </div>
+                <div className="grid-container">
+                {media.map((media, i) => (
+                <div key={i} className="grid-item">
+                        <img src={`https://yilvmajrrjkkeljiduxi.supabase.co/storage/v1/object/public/Works/${user.id}/${media.name}`}
+                                onClick={() => window.open(`https://yilvmajrrjkkeljiduxi.supabase.co/storage/v1/object/public/Works/${user.id}/${media.name}`, '_blank')}/>
+                    <div className="button-container">
+                    <button className="delete-button" onClick={() => deleteMedia(media.name)}>Delete Image</button>
+                    <button className="download-button" onClick={() => downloadMedia(media.name)}>Download Image</button>
+                    </div>
+                </div>
+                ))}
+                </div>
+        </div>
+          
     );
 }
-
-
-
 
 export default Works;
